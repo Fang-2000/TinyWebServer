d1b917680cb3c2769bb0b6038e740f66469d804f
f <862237572@qq.com>
2024-01-25 19:09:04 +0800

    threadpool

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -13,8 +13,10 @@ class threadpool
 {
 public:
     /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
+    //connPool是数据库连接池指针
     threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
     ~threadpool();
+    //向请求队列中插入任务请求
     bool append(T *request, int state);
     bool append_p(T *request);
 
@@ -38,16 +40,19 @@ threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int threa
 {
     if (thread_number <= 0 || max_requests <= 0)
         throw std::exception();
+    //线程id初始化
     m_threads = new pthread_t[m_thread_number];
     if (!m_threads)
         throw std::exception();
     for (int i = 0; i < thread_number; ++i)
     {
+        //循环创建线程，并将工作线程按要求进行运行
         if (pthread_create(m_threads + i, NULL, worker, this) != 0)
         {
             delete[] m_threads;
             throw std::exception();
         }
+        //将线程进行分离后，不用单独对工作线程进行回收
         if (pthread_detach(m_threads[i]))
         {
             delete[] m_threads;
@@ -64,14 +69,17 @@ template <typename T>
 bool threadpool<T>::append(T *request, int state)
 {
     m_queuelocker.lock();
+    //根据硬件，预先设置请求队列的最大值
     if (m_workqueue.size() >= m_max_requests)
     {
         m_queuelocker.unlock();
         return false;
     }
     request->m_state = state;
+    //添加任务
     m_workqueue.push_back(request);
     m_queuelocker.unlock();
+    //信号量提醒有任务要处理
     m_queuestat.post();
     return true;
 }
@@ -92,6 +100,7 @@ bool threadpool<T>::append_p(T *request)
 template <typename T>
 void *threadpool<T>::worker(void *arg)
 {
+    //将参数强转为线程池类，调用成员方法
     threadpool *pool = (threadpool *)arg;
     pool->run();
     return pool;
@@ -101,13 +110,19 @@ void threadpool<T>::run()
 {
     while (true)
     {
+        //信号量等待
         m_queuestat.wait();
+
+        //被唤醒后先加互斥锁
         m_queuelocker.lock();
         if (m_workqueue.empty())
         {
             m_queuelocker.unlock();
             continue;
         }
+
+        //从请求队列中取出第一个任务
+        //将任务从请求队列删除
         T *request = m_workqueue.front();
         m_workqueue.pop_front();
         m_queuelocker.unlock();
@@ -120,6 +135,7 @@ void threadpool<T>::run()
                 if (request->read_once())
                 {
                     request->improv = 1;
+                    //从连接池中取出一个数据库连接
                     connectionRAII mysqlcon(&request->mysql, m_connPool);
                     request->process();
                 }
@@ -144,7 +160,9 @@ void threadpool<T>::run()
         }
         else
         {
+            //将数据库连接放回连接池
             connectionRAII mysqlcon(&request->mysql, m_connPool);
+            //process(模板类中的方法,这里是http类)进行处理
             request->process();
         }
     }
